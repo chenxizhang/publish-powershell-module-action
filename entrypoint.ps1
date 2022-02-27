9192089ff41eda39c537659d4a12575da4b51904
chenxizhang <ares@xizhang.com>
2022-02-27 17:48:55 +0800

    Update entrypoint.ps1

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -2,14 +2,14 @@
 
 Write-Host $env:INPUT_MODULEPATH
 Write-Host $env:INPUT_NUGETAPIKEY
+Write-Host $env:INPUT_DEPENDENCIES
 
 Get-ChildItem -Path env:
 Write-Host -Object ('Publishing module ({0}) to PowerShell Gallery' -f $env:INPUT_MODULEPATH)
-
-Get-ChildItem -Path $env:GITHUB_WORKSPACE | Format-Table -AutoSize
-Get-Module | Format-Table -AutoSize
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module code365scripts.teams,code365scripts.weixin
+
+if()
+Install-Module 
 
 Publish-Module -Path $env:INPUT_MODULEPATH -NuGetApiKey $env:INPUT_NUGETAPIKEY
 Write-Host -Object 'Finished publishing module to PowerShell Gallery'
